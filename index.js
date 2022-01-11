930ebd564fc0161e0d4604eda369a4b22228e3ce
Jean Gabriel Ferreira <jeangabrielferreira1@gmail.com>
2022-01-11 15:59:56 -0300

    Leitura de arquivo e tratamento de erros

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 import chalk from 'chalk';
+import fs from 'fs';
 
-console.log(chalk.red('Jean Gabriel Ferreira'));
+function exception(error) {
+    throw new Error(chalk.red(error));
+};
+
+function getFile(path) {
+    const enconding = 'utf-8';
+    fs.readFile(path, enconding, function (error, data) {
+        if (error)
+            exception(error);
+        else
+            console.log(chalk.green(data));
+    });
+};
+
+getFile('./files/texto.md');
+
