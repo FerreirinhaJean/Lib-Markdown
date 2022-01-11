cf90c1c9e9a2889212d776d87519172d401702aa
Jean Gabriel Ferreira <jeangabrielferreira1@gmail.com>
2022-01-11 16:37:14 -0300

    Utilizando async/await

--- index.js
+++ index.js
@@ -5,16 +5,29 @@ function exception(error) {
     throw new Error(chalk.red(error));
 };
 
-function getFile(path) {
-    const enconding = 'utf-8';
-    fs.promises.readFile(path, enconding)
-        .then((data) => {
-            console.log(chalk.green(data));
-        })
-        .catch((error) => {
-            exception(error);
-        });
+//Utilizando Async/Await
+async function getFile(path) {
+    try {
+        const enconding = 'utf-8';
+        const text = await fs.promises.readFile(path, enconding);
+
+        console.log(chalk.green(text));
+    } catch (error) {
+        exception(error);
+    }
 };
+
+//Utilizando Promisse com Then()
+// function getFile(path) {
+//     const enconding = 'utf-8';
+//     fs.promises.readFile(path, enconding)
+//         .then((data) => {
+//             console.log(chalk.green(data));
+//         })
+//         .catch((error) => {
+//             exception(error);
+//         });
+// };
 
 getFile('./files/texto.md');
 
