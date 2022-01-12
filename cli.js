f80f942e13076cbd829ae23ff239cdcccbc83004
Jean Gabriel Ferreira <jeangabrielferreira1@gmail.com>
2022-01-12 14:52:40 -0300

    Ajuste do resultado do objeto

--- cli.js
+++ cli.js
@@ -8,7 +8,7 @@ async function processText(path) {
     const result = await getFile(path[2]);
 
     if (path[3] === 'validar')
-        console.log('links validados', await validateLinks(result));
+        console.log(chalk.yellow('links validados'), await validateLinks(result));
     else
         console.log(chalk.yellow('Lista de links'), result);
 };
--- http-validacao.js
+++ http-validacao.js
@@ -1,12 +1,20 @@
 import fetch from "node-fetch";
 
-async function checkStatus(urls) {
-    const status = await Promise.all(urls.map(async url => {
-        const response = await fetch(url);
-        return response.status;
-    }));
+function exception(error) {
+    throw new Error(error.message);
+};
 
-    return status;
+async function checkStatus(urls) {
+    try {
+        const status = await Promise.all(urls.map(async url => {
+            const response = await fetch(url);
+            return response.status;
+        }));
+
+        return status;
+    } catch (error) {
+        exception(error);
+    }
 };
 
 function createArrayLinks(links) {
@@ -14,10 +22,15 @@ function createArrayLinks(links) {
 };
 
 async function validateLinks(links) {
-    const result = createArrayLinks(links);
-    const statusLinks = await checkStatus(result);
+    const arrayLinks = createArrayLinks(links);
+    const statusLinks = await checkStatus(arrayLinks);
 
-    return statusLinks;
+    const result = links.map((link, index) => ({
+        ...link,
+        status: statusLinks[index]
+    }));
+
+    return result;
 };
 
 export { validateLinks };
